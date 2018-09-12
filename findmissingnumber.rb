30316f11a03ac327f3df1800408d0c7c16edb5c0
Blake DeStasio <38029220+Bdestasio33@users.noreply.github.com>
2018-09-12 13:45:21 -0400

    Finds a missing number in sequence

---
+++ findmissingnumber.rb
@@ -0,0 +1,38 @@
+
+keeplooping = true
+puts "what is your string? input as 1,2,3, etc."
+number_str = gets.chomp
+number_arr = number_str.split(',')
+number_arr.sort
+maxvalue = number_arr.max
+(maxvalue.to_i)
+x=0
+j=1
+putsanswer = false
+while (keeplooping)
+    
+   if ((number_arr[j].to_i) != ((number_arr[x].to_i) + 1))
+     answer = ((number_arr[x].to_i) + 1)
+     keeplooping = false
+    end
+    
+    if answer == ((maxvalue.to_i) + 1)
+        puts "You didnt even skip a number!"
+    end
+    if answer != ((maxvalue.to_i) + 1)
+        puts answer
+    end
+   
+   x += 1
+   j += 1
+end
+
+ 
+
+
+
+
+
+
+
+
